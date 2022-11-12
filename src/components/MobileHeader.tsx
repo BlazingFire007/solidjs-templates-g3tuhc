2db0e56549d396722f3df034bdb6ae60f283872f
Eli Richardson <elijahrich02@icloud.com>
2022-11-12 17:27:29 +0000

    Update 4 files

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -1,19 +1,26 @@
+import Nav from './Nav';
+
 export default function MobileHeader() {
   function showNav() {
-    const nav = document.querySelector('main');
-    if (nav) nav.classList.toggle('show-nav');
+    const main = document.querySelector('main');
+    const header = document.querySelector('header');
+    if (main) main.classList.toggle('show-nav');
+    if (header) header.classList.toggle('show-nav');
   }
   return (
-    <section id="header">
+    <section id='header'>
       <header>
-        <p id="logo">
-          <a href="#">Eli Richardson</a>
+        <p id='logo'>
+          <a href='#'>Eli Richardson</a>
         </p>
         <div>
           <button onClick={showNav}>
-            <i class="fa fa-bars"></i>
+            <i class='fa fa-bars'></i>
           </button>
         </div>
+        <aside>
+          <Nav />
+        </aside>
       </header>
     </section>
   );
